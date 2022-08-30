ce35028e0f2bbc83c3562e7c7ca6508043c146ff
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-08-30 23:47:53 +0200

    add dropdown component

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,17 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
+$('.wrap').html(
+  `
+       <div class="dropdown">
+         <button class="btn btn-primary dropdown-toggle" id="dropDownMenuButton">Drop down</button>
+
+         <div class="dropdown-menu" data-toggle-id="dropDownMenuButton">
+             <a class="dropdown-item" href="#">Action</a>
+             <a class="dropdown-item" href="#">Action#2</a>
+             <a class="dropdown-item" href="#">Action#3</a>
+         </div>
+       </div> 
+  `
+);
+$('.dropdown-toggle').dropdown();
